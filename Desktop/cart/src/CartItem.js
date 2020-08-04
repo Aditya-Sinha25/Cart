56c3df49bdd6cadd54ab2135f35bc6e3cd0f7eb0
Aditya <adityagodofgamer.1@gmail.com>
2020-08-04 14:01:24 +0530

    inc and dec added in the cart

--- Desktop/cart/src/CartItem.js
+++ Desktop/cart/src/CartItem.js
@@ -1,20 +1,61 @@
 import React from 'react';
 
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state= {
+            price:999,
+            title:'Phone',
+            qty:1
+        }
+    }
+        increaseQuantity = () =>{
+            this.setState((prevState)=>{
+                return{
+                    qty:prevState.qty+1
+                }
+            });
+        }
+        decreaseQuantity = () =>{
+            this.setState((prevState)=>{
+                return{
+                    qty:prevState.qty-1
+                }
+            });
+        }
     render(){
         return(
-            <div className="card-item">
+            <div className="cart-item">
                 <div className="left-block">
-
+                    <img style={styles.images} />
                 </div>
                 <div className="right-block">
-                    <div>Phone</div>
-                    <div>Rs 999</div>
-                    <div>Qty:1</div>
-                    <div className="cart-item-actions"></div>
+                    <div style={{fontSize:25}}>{this.state.title}</div>
+                    <div style={{color:'777'}}>Rs:{this.state.price}</div>
+                    <div style={{fontSize:25}}>Qty:{this.state.qty}</div>
+                    <div className="cart-item-actions">
+                        <img alt="increase" 
+                            className="action-icons" 
+                            src="https://image.flaticon.com/icons/svg/992/992651.svg" 
+                            onClick={this.increaseQuantity} />
+                        <img alt="decrease" 
+                            className="action-icons" 
+                            src="https://image.flaticon.com/icons/svg/992/992683.svg"
+                            onClick={this.decreaseQuantity} />
+                        <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/svg/1345/1345823.svg" />
+                    </div>
                 </div>
             </div>
         );
+    }
+}
+
+const styles ={
+    images:{
+        height:110,
+        width:110,
+        borderRadius:4,
+        background: '#ccc'
     }
 }
 
